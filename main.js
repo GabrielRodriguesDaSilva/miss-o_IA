bb43c974c63f8bef2dffca79a9558d6b7cf7a4db
GabrielRodriguesDaSilva <105791228+GabrielRodriguesDaSilva@users.noreply.github.com>
2024-06-06 11:00:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = documento.querySelector('.caixa-pricipal');
+const caixaPergunta = documento.querySelector('.caixa-pergunta');
+const caixaAlternativa = documento.querySelector('.caixa-alternativa');
+const caixaResultado = documento.querySelector('.caixa-resultado');
+const caixaResultado = documento.querySelector('.caixa-resultado');
